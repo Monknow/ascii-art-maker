9c77f6b33127626d854c98b79f04f73e4d1716e4
Monknow <juanrodriguezcode@gmail.com>
2021-10-06 06:47:49 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "ASCII Images Converter",
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"],
+};
